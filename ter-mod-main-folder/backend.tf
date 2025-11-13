799fd34ec06390d20ce97e42b3477bc540e8c9f3
printo varghese <printovarghese09@gmail.com>
2025-11-13 20:45:11 +0530

    Delete ter-mod-main-folder directory

--- ter-mod-main-folder/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-backend-printoportfolio"
-    key            = "global/s3/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
-}
